b98189a0a8edb4defbddcad17b9af02922b5edce
jimneylee <jimney@163.com>
2013-12-09 15:14:42 +0800

    add comment

--- JLNimbusTimeline/Controllers/JLNimbusTableViewController.m
+++ JLNimbusTimeline/Controllers/JLNimbusTableViewController.m
@@ -88,6 +88,7 @@
     // Dispose of any resources that can be recreated.
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
